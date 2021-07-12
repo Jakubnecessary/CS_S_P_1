928e5357c2681e7807a7ac39dbc3d06673705ba0
jakubnecessary <cabbage1991@gmail.com>
2021-07-12 13:32:45 +0100

    supplier tested

---
+++ tests/supplier_test.py
@@ -0,0 +1,12 @@
+import unittest 
+from models.supplier import Supplier
+
+class TestSupplier(unittest.TestCase):
+    def setUp(self):
+        self.supplier = Supplier("Chappels Craft" , "England")
+
+    def test_supplier_has_a_name(self):
+        self.assertEqual("Chappels Craft", self.supplier.company_name)
+
+    def test_supplier_has_origin(self):
+        self.assertEqual("Chappels Craft", self.supplier.company_origin)
