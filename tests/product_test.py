423b33f0e6577eb0f0e1b5c04ffdd5e050ed4939
jakubnecessary <cabbage1991@gmail.com>
2021-07-12 13:26:42 +0100

    Product Tested

--- tests/product_test.py
+++ tests/product_test.py
@@ -9,3 +9,9 @@ class TestProduct(unittest.TestCase):
     
     def test_product_has_a_type(self):
         self.assertEqual("Small Candle", self.product.product_type)
+
+    def test_product_has_a_quantity(self):
+        self.assertEqual(15, self.test_product_has_a_quantity)
+        
+    def test_product_has_a_price(self):
+        self.assertEqual(5, self.product.selling_price)
