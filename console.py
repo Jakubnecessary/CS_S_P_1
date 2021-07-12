1d1a2326ef206adc70373e234483ad6553af4695
jakubnecessary <cabbage1991@gmail.com>
2021-07-12 16:01:03 +0100

    Added createnewproduct funtion/added import request and made a post form request

--- console.py
+++ console.py
@@ -7,11 +7,11 @@ import repositories.supplier_repository as supplier_repository
 
 
 supplier_1 = Supplier("England", "Smbullion")
-
+supplier_repository.save(supplier_1)
 supplier_2 = Supplier("Scotland", "CandleShack")
-
+supplier_repository.save(supplier_2)
 
 product_1 = Product("Candle_2", "Big Candle", "Handmade Big Candle", 10, 12, supplier_2)
-
+product_repository.save(product_1)
 product_2 = Product("Straw_1", "Small Set", "Small Set of handmade straws", 15, 3, supplier_1)
-
+product_repository.save(product_2)
--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.product import Product
 import repositories.supplier_repository as supplier_repository
@@ -18,3 +18,17 @@ def products():
 def new_product():
     suppliers = supplier_repository.select_all()
     return render_template('products/new_product.html', all_suppliers = suppliers)
+
+# POST '/products'
+@products_blueprint.route("/products", methods=['POST'])
+def create_new_product():
+    product_name = request.form['product_name']
+    product_type = request.form['product_type']
+    product_description = request.form['product_description']
+    stock_quantity = request.form['stock_quantity']
+    selling_price = request.form['selling_price']
+    supplier  = supplier_repository.select(request.form['author_id'])
+    product = Product(product_name, product_type, product_description,stock_quantity, selling_price, supplier)
+    product_repository.save(product)
+    return redirect('/products')
+    
